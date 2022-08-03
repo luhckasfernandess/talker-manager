5d07c2c4c0f6c1d3284ffcc8123306764e61f738
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-08-03 16:46:05 -0400

    feat: requirement2: Create endpoint GET /talker/:id

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const talker = async () => {
+  const talkerFile = await fs.readFile('./talker.json', 'utf-8');
+  return JSON.parse(talkerFile);
+};
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -19,8 +24,19 @@ app.listen(PORT, () => {
 
 app.get('/talker', async (request, response) => {
   try {
-    const talker = await fs.readFile('./talker.json', 'utf8');
-    response.status(200).json(JSON.parse(talker));
+    response.status(200).json(await talker());
+  } catch (error) {
+    response.status(500).json(error);
+  }
+});
+
+app.get('/talker/:id', async (request, response) => {
+  try {
+    const { id } = request.params;
+    const talkerList = await talker();
+    const talkerId = talkerList.find((t) => t.id === Number(id));
+    if (!talkerId) response.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    response.status(200).json(talkerId);
   } catch (error) {
     response.status(500).json(error);
   }
